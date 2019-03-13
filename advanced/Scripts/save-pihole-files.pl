378441d2dc8fa8d0e1c6c3e1fe1c98c291e467e2
maximilien-laude <48359671+maximilien-laude@users.noreply.github.com>
2019-03-13 22:25:28 +0100

    Update save-pihole-files.pl

--- advanced/Scripts/save-pihole-files.pl
+++ advanced/Scripts/save-pihole-files.pl
@@ -8,26 +8,22 @@ my $save_dir = "/root/saves";
 my $pihole_config_tar_gz = "/root/saves/pihole-configs.tar.gz";
 my $pihole_system_tar_gz = "/root/saves/pihole-system.tar.gz";
 my $pihole_number_words = "/var/lib/pihole-system/etc/pihole/totals-domains.txt";
-my $pihole_white_list = '/var/lib/pihole-system/etc/pihole/blacklist.txt';
-my $pihole_black_list = '/var/lib/pihole-system/etc/pihole/whitelist.txt';
-my $pihole_regex_list = '/var/lib/pihole-system/etc/pihole/regex.list';
+my $pihole_white_list = "/var/lib/pihole-system/etc/pihole/blacklist.txt";
+my $pihole_black_list = "/var/lib/pihole-system/etc/pihole/whitelist.txt";
+my $pihole_regex_list = "/var/lib/pihole-system/etc/pihole/regex.list";
 
 sub total_words {
 
-	my $pihole_number_words_testing = shift ;
-	my $pihole_list_w = shift ;
-	my $pihole_list_b = shift ;
-	my $pihole_list_rx = shift ;
+        my $a = shift;
+        open(FILE, "<$a");
+        my $words = "0";
+        while (<FILE>) {
 
-	my @pihole_lists_wb = ( $pihole_list_w , $pihole_list_b , $pihole_list_rx );
-        my $total=`wc -l @pihole_lists_wb | tail -n1 `;
-        $total =~ s/^\s+|\s+$//g;
-	$total =~ s/total//;        
-	$total =~ s/^\s+|\s+$//g;
-       	open(my $fw, '>', $pihole_number_words_testing );
-        print $fw "$total\n";
-        close $fw;;
+                $words++;
 
+        }
+        close(FILE);
+        return $words;
 }
 
 sub file_content {
@@ -38,7 +34,28 @@ sub file_content {
 
 }
 
+sub save_total {
 
+	my $file_to_write = shift;
+	my $total_increment = "0";
+	my $list;
+	my $pihole_white_list = shift;
+	my $pihole_black_list = shift;
+	my $pihole_regex_list = shift;
+	my @pihole_lists_wb = ( $pihole_white_list , $pihole_black_list , $pihole_regex_list );
+	
+	foreach $list ( @pihole_lists_wb  ) {
+
+		my $total = total_words("$list");
+       		$total_increment += $total ;
+				
+    	}
+
+    	open(my $fw, '>', "$file_to_write" );
+    	print $fw "$total_increment";
+    	close $fw;
+
+}
 
 if ( ! -d $save_dir ) {
 
@@ -60,11 +77,9 @@ if ( ! -e $pihole_system_tar_gz ){
 ######### pihole-config ############
 ####################################
 
-
-
 if ( ! -e $pihole_config_tar_gz ){
 	
-        system("tar -zcf $pihole_config_tar_gz /var/lib/pihole-system/etc/pihole");
+    system("tar -zcf $pihole_config_tar_gz /var/lib/pihole-system/etc/pihole");
 	
 }
 
@@ -74,27 +89,31 @@ elsif ( -e $pihole_config_tar_gz ){
 
 		my $pihole_number_words_save="/var/lib/pihole-system/etc/pihole/totals-domains-saved.txt";
 		my $pihole_number_words_testing="/tmp/totals-domains-testing.txt";	
-	
+			
+
 		if( ! -e $pihole_number_words_save ){
 		
-			unlink $pihole_config_tar_gz ;
-			total_words($pihole_number_words_save , $pihole_white_list , $pihole_black_list , $pihole_regex_list );
-		
+			unlink $pihole_config_tar_gz;		
+			save_total($pihole_number_words_save,$pihole_white_list,$pihole_black_list,$pihole_regex_list);
+
 		}
 		
 		else {
 		
-			total_words($pihole_number_words_testing , $pihole_white_list , $pihole_black_list , $pihole_regex_list );
-			
 			my $saved_domains_count = file_content($pihole_number_words_save);
+			save_total($pihole_number_words_testing,$pihole_white_list,$pihole_black_list,$pihole_regex_list);                     
 			my $testing_domains_count = file_content($pihole_number_words_testing);
-			
+
 			if ( $saved_domains_count != $testing_domains_count ) {
-			
+							
 				unlink $pihole_config_tar_gz ;
-                                total_words($pihole_number_words_save , $pihole_white_list , $pihole_black_list , $pihole_regex_list );
+				unlink $saved_domains_count ;
+                    		open(my $fw, '>', "$pihole_number_words_save");
+				print $fw "$testing_domains_count";
+                                close $fw;
 
 			}
+			
 			
 			else {
 			
@@ -112,7 +131,7 @@ elsif ( -e $pihole_config_tar_gz ){
 
 	else {
 	
-		exit 0
+		die "no pihole lists can't save";
 	
 	}	
 	 
