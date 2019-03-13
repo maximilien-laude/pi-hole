62a9c4c31e578e28c251580b74d836e87f47ab4b
maximilien-laude <48359671+maximilien-laude@users.noreply.github.com>
2019-03-13 22:53:33 +0100

    Update save-pihole.sh

--- advanced/Scripts/save-pihole.sh
+++ advanced/Scripts/save-pihole.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-mount -o remount,rw /
-perl /lib/systemd/system-shutdown/save-pihole-files.pl
-mount -o remount,ro /
+if [[ -e "/lib/systemd/system-shutdown/save-pihole-files.pl" ]]
+
+        echo "can't save pihole !"
+        exit 1
+
+else
+
+        mount -o remount,rw /
+        perl /lib/systemd/system-shutdown/save-pihole-files.pl
+        mount -o remount,ro /
+        exit 0
+
+fi
